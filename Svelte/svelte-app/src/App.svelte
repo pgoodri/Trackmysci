b7e691c54fcd937e227fb2baed28c3076c295817
Paige Goodrich <paigegoodrich03@gmail.com>
2024-09-23 14:09:29 -0500

    Adding all base files for Firebase and Svelte. Please note that there are items in App.svelte and Success.svelte that are to be used for starting the re-routing process, but those items can be edited.

---
+++ Svelte/svelte-app/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { Router, Route, navigate } from 'svelte-routing';
+    import Success from './Success.svelte';
+
+    function testNavigate() {
+		console.log("Navigating to success page");
+    	navigate('/success');
+    	console.log("Navigate function called");
+    }
+</script>
+
+<Router>
+    <main>
+        <h1>Welcome!</h1>
+        <button on:click={testNavigate}>Go to Success</button>
+    </main>
+
+    <Route path="/success" component={Success} />
+</Router>
