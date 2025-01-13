5bb2363d6a61efa9cb5649510534a9ab5d5c88ab
alyab0uzaid <aabouza@siue.edu>
2025-01-13 10:31:15 -0600

    change userStore.js

--- Svelte/svelte-app/src/userStore.js
+++ Svelte/svelte-app/src/userStore.js
@@ -1,3 +1,13 @@
-import { writable } from 'svelte/store';
+import { writable } from "svelte/store";
+import { auth, onAuthStateChanged } from "./firebase";
 
-export const userStore = writable(null); // Store to hold logged-in user data
+export const userStore = writable(null);
+
+// Track authentication state
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        userStore.set(user); // Set the user in the store
+    } else {
+        userStore.set(null); // Clear the user when logged out
+    }
+});
