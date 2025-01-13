1a34d716c6353cd9dc3751a156fdbca3c8561021
alyab0uzaid <aabouza@siue.edu>
2025-01-13 10:25:20 -0600

    update

--- Svelte/svelte-app/src/firebase.js
+++ Svelte/svelte-app/src/firebase.js
@@ -1,7 +1,9 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-
+// Your Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCHdf8tVDVOtTazjvC0h1PyKwqNifWfqww",
     authDomain: "trackmysci.firebaseapp.com",
@@ -12,8 +14,9 @@ const firebaseConfig = {
     measurementId: "G-6JR45C2DBF",
 };
 
-// Initialize Firebase
+// Initialize Firebase app and Firestore
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export { db, auth, onAuthStateChanged };
