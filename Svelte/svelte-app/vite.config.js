3a05fc579abad5af1731b9d282297ff99af55605
Paige Goodrich <paigegoodrich03@gmail.com>
2025-01-15 21:19:50 -0600

    Firestore basics creating collection for users to save firstName, lastName, and email

--- Svelte/svelte-app/vite.config.js
+++ Svelte/svelte-app/vite.config.js
@@ -4,6 +4,10 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    outDir: "dist", // Specify the output directory
+    emptyOutDir: true, // Clears the output directory before each build
+  },
   resolve: {
     alias: {
       $lib: path.resolve('./src/lib'), 
