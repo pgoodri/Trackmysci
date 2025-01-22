1b87369f0466f42c23cac3ad95bb5e6f2168fb0b
Jacob Craven <jacobtcraven02@gmail.com>
2025-01-22 09:27:17 -0600

    Updated location of dist folder after build

--- Svelte/svelte-app/vite.config.js
+++ Svelte/svelte-app/vite.config.js
@@ -9,4 +9,15 @@ export default defineConfig({
       $lib: path.resolve('./src/lib'), 
     },
   },
+  build: {
+    outDir: '../../dist', // Replace with your desired output folder name
+    // assetsDir: 'assets',         // Folder for storing built JS/CSS assets
+    // rollupOptions: {
+    //   output: {
+    //     assetFileNames: 'assets/[name].[hash][extname]', // For hashed asset names
+    //     chunkFileNames: 'assets/[name].[hash].js',
+    //     entryFileNames: 'assets/[name].[hash].js',
+    //   },
+    // },
+  },
 });
